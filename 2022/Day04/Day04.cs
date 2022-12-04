b441225b5ab7be788b176bdc87adb231e93e48a0
Mateusz Skapski <mateusz.skapski@iris.co.uk>
2022-12-04 14:05:12 +0000

    Day 4 solution.

---
+++ 2022/Day04/Day04.cs
@@ -0,0 +1,46 @@
+public class Day04 : Problem
+{
+    public Day04(int year, int day, string fileName) : base(year, day, fileName)
+    {
+    }
+
+    protected override object PartOne(List<string> input)
+    {
+        var containerRangeCount = 0;
+        foreach (var line in input)
+        {
+            var pairs = line.Split(",");
+            var left = GetRange(pairs[0]);
+            var right = GetRange(pairs[1]);
+
+            if ((right.Start >= left.Start && right.End <= left.End) || 
+                (left.Start >= right.Start && left.End <= right.End))
+                containerRangeCount++;
+        }
+        
+        return containerRangeCount;
+    }
+
+    protected override object PartTwo(List<string> input)
+    {
+        var doNotOverlap = 0;
+        foreach (var line in input)
+        {
+            var pairs = line.Split(",");
+            var left = GetRange(pairs[0]);
+            var right = GetRange(pairs[1]);
+            
+            if ((left.End < right.Start || left.Start > right.End))
+                doNotOverlap++;
+        }
+        
+        return input.Count - doNotOverlap;
+    }
+
+    (int Start, int End) GetRange(string pair)
+    {
+        var split = pair.Split("-").Select(x => int.Parse(x));
+
+        return (split.ElementAt(0), split.ElementAt(1));
+    }
+}
